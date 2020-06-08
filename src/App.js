0a88ae084b2f8e119f845e432e2e9750abb2a68b
Armin Kovacs <armin0191@gmail.com>
2020-06-08 14:43:10 +0200

    Add NavBar foundation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Layout, Menu } from "antd";
 
 function App() {
+  const { Content, Footer } = Layout;
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Layout className="layout">
+          <div className="logo" />
+          <Menu theme="light" mode="horizontal">
+            <Menu.Item key="1">Home</Menu.Item>
+            <Menu.Item key="2">Holiday</Menu.Item>
+            <Menu.Item key="3">Shifts</Menu.Item>
+          </Menu>
+        <Content style={{ padding: "0 50px" }}>
+          <div className="site-layout-content">
+
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          ©2020 Created by Ármin Kovács
+        </Footer>
+      </Layout>
     </div>
   );
 }
