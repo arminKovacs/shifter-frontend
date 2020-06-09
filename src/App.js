4764b72b9be790299cab38871247c9ce10c543f0
Armin Kovacs <armin0191@gmail.com>
2020-06-09 14:00:51 +0200

    Set navbar with logo first iteration

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from "react";
 import "./App.css";
 import { Layout, Menu } from "antd";
+import styled from "styled-components";
+import logo from "./images/logo.png";
+
+let Logo = styled.div`
+  background-image: url(${logo});
+  height: 42%;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-size: contain;
+  position: relative;
+  vertical-align: bottom;
+  display: inline-block;
+  top: 1px;
+  border-bottom: 2px solid transparent;
+`;
 
 function App() {
-  const { Content, Footer } = Layout;
+  const { Header, Content, Footer } = Layout;
 
   return (
     <div className="App">
       <Layout className="layout">
-          <div className="logo" />
+        <Header className="header">
+          <Logo/>
           <Menu theme="light" mode="horizontal">
             <Menu.Item key="1">Home</Menu.Item>
             <Menu.Item key="2">Holiday</Menu.Item>
             <Menu.Item key="3">Shifts</Menu.Item>
           </Menu>
-        <Content style={{ padding: "0 50px" }}>
-          <div className="site-layout-content">
-
-          </div>
+        </Header>
+        <Content className="mainContent">
+          <div className="site-layout-content"></div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          ©2020 Created by Ármin Kovács
-        </Footer>
+        <Footer className="footer">©2020 Created by Ármin Kovács</Footer>
       </Layout>
     </div>
   );
