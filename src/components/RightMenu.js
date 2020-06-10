cb523ed36d5c81ed696b1a59d390164c72cb2d0b
Armin Kovacs <armin0191@gmail.com>
2020-06-10 10:57:33 +0200

    Change menu item to not be selected on click

--- src/components/RightMenu.js
+++ src/components/RightMenu.js
@@ -5,7 +5,7 @@ export default function RightMenu(props) {
   let modeState = props.modeProp;
 
   return (
-    <Menu mode={modeState}>
+    <Menu mode={modeState} selectedKeys={0}>
       <Menu.Item key="signin">Signin</Menu.Item>
       <Menu.Item key="signup">Signup</Menu.Item>
     </Menu>
