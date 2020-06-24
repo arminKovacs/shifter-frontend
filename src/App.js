994aff35506b80ffe8676e0f237ed0718cdcf781
Armin Kovacs <armin0191@gmail.com>
2020-06-24 09:44:21 +0200

    Create placeholder main components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home"
+import Planner from "./components/Planner"
+import Shifts from "./components/Shifts"
+import Requests from "./components/Requests"
 
 function App() {
   return (
---
+++ src/components/Requests.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Requests() {
+  return <div>This is the requests</div>;
+}
