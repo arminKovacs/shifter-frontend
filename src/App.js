8eb8b968dbb1d3a00ff4ec43bae7b603eb0eb19f
Armin Kovacs <armin0191@gmail.com>
2020-06-24 11:34:52 +0200

    Create signin and signup placeholder, fix scrollbar bug in navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./css/App.css";
 import NavBar from "./components/navbar_parts/NavBar";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import Home from "./components/main/Home"
-import Planner from "./components/main/Planner"
-import Shifts from "./components/main/Shifts"
-import Requests from "./components/main/Requests"
+import Home from "./components/main/Home";
+import Planner from "./components/main/Planner";
+import Shifts from "./components/main/Shifts";
+import Requests from "./components/main/Requests";
+import Signin from "./components/main/Signin";
+import Signup from "./components/main/Signup";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/planner" component={Planner} />
         <Route path="/shifts" component={Shifts} />
         <Route path="/requests" component={Requests} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
       </Router>
     </div>
   );
