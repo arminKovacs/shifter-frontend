d3ff82d584d9882a3f4ce8e95fbfc75369e672d1
Armin Kovacs <armin0191@gmail.com>
2020-07-30 12:00:34 +0200

    Change requests display design

--- src/components/pages/Requests.js
+++ src/components/pages/Requests.js
@@ -1,5 +1,60 @@
 import React from "react";
+import { Table, Space } from 'antd';
 
 export default function Requests() {
-  return <div>This is the requests</div>;
+  const dataSource = [
+    {
+      key: "1",
+      name: "Test Joe",
+      shiftName: "Holiday",
+      shiftTime: "00:00:00 - 23:59:59",
+      shiftDate: "2020-08-20 - 2020-08-23",
+    },
+    {
+      key: "2",
+      name: "Test Bob",
+      shiftName: "Morning",
+      shiftTime: "06:00:00 - 14:00:00",
+      shiftDate: "2020-08-20 - 2020-08-23",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Shift name",
+      dataIndex: "shiftName",
+      key: "shiftName",
+    },
+    {
+      title: "Shift time",
+      dataIndex: "shiftTime",
+      key: "shiftTime",
+    },
+    {
+      title: "Shift date",
+      dataIndex: "shiftDate",
+      key: "shiftDate",
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <button>Accept</button>
+          <button>Deny</button>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <Table dataSource={dataSource} columns={columns} scroll={{ x: 'max-content' }} />;
+    </div>
+  );
 }
